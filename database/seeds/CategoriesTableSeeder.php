0ec90bb2cb14982f44b614cdfe1e004be90116f7
Alex <alexk.mammut@gmail.com>
2017-12-02 16:39:11 +0200

    Create base subcategory

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Category;
 use Illuminate\Database\Seeder;
 
 class CategoriesTableSeeder extends Seeder
@@ -11,32 +12,33 @@ class CategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        \App\Category::create([
+        Category::query()->truncate();
+        Category::create([
             'name' => 'Косметика для лица и области шеи',
             'photo' => '/images/categories/Категория_ косметика для лица2.png',
             'url' => 'kosmetika-dlya-litsa-i-oblasti-shei',
         ]);
-        \App\Category::create([
+        Category::create([
             'name' => 'Уход и очищение кожи лица',
             'photo' => '/images/categories/Категория_уход_и_очищение3.png',
             'url' => 'uhod-i-ochischenie-kozhi-litsa',
         ]);
-        \App\Category::create([
+        Category::create([
             'name' => 'Косметика для тела',
             'photo' => '/images/categories/Категория_косметика_для_тела.png',
             'url' => 'kosmetika-dlya-tela',
         ]);
-        \App\Category::create([
+        Category::create([
             'name' => 'Косметика для волос',
             'photo' => '/images/categories/Категория_для_волос.png',
             'url' => 'kosmetika-dlya-volos',
         ]);
-        \App\Category::create([
+        Category::create([
             'name' => 'Мыло и бельди',
             'photo' => '/images/categories/Категория_домашнее_мыло.png',
             'url' => 'mylo-i-beldi',
         ]);
-        \App\Category::create([
+        Category::create([
             'name' => 'Подарочные наборы и акции',
             'photo' => '/images/categories/Категория_Наборы_акция.png',
             'url' => 'podarochnye-nabory-i-aktsii',
--- database/seeds/SubCategoriesTableSeeder.php
+++ database/seeds/SubCategoriesTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\SubCategory;
 use Illuminate\Database\Seeder;
 
 class SubCategoriesTableSeeder extends Seeder
@@ -11,6 +12,102 @@ class SubCategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(\App\SubCategory::class, 20)->create();
+        SubCategory::query()->truncate();
+        SubCategory::create([
+            'category_id' => 1,
+            'title' => 'Легкая серия',
+            'url' => 'legkaya-seriya',
+        ]);
+        SubCategory::create([
+            'category_id' => 1,
+            'title' => 'Серия Актив',
+            'url' => 'seriya-aktiv',
+        ]);
+        SubCategory::create([
+            'category_id' => 1,
+            'title' => 'Пептидная серия',
+            'url' => 'peptidnaya-seriya',
+        ]);
+        SubCategory::create([
+            'category_id' => 1,
+            'title' => 'Солнцезащитная серия',
+            'url' => 'solntsezaschitnaya-seriya',
+        ]);
+        SubCategory::create([
+            'category_id' => 1,
+            'title' => 'Другое',
+            'url' => 'drugoe',
+        ]);
+
+        SubCategory::create([
+            'category_id' => 2,
+            'title' => 'Средства для снятия макияжа',
+            'url' => 'sredstva-dlya-snyatiya-makiyazha',
+        ]);
+        SubCategory::create([
+            'category_id' => 2,
+            'title' => 'Скрабы пилинги маски',
+            'url' => 'skraby-pilingi',
+        ]);
+        SubCategory::create([
+            'category_id' => 2,
+            'title' => 'Убтан',
+            'url' => 'ubtan',
+        ]);
+
+        SubCategory::create([
+            'category_id' => 3,
+            'title' => 'Крема',
+            'url' => 'krema',
+        ]);
+        SubCategory::create([
+            'category_id' => 3,
+            'title' => 'Скрабы',
+            'url' => 'skraby',
+        ]);
+        SubCategory::create([
+            'category_id' => 3,
+            'title' => 'Антицеллюлитная косметика',
+            'url' => 'antitsellyulitnaya-kosmetika',
+        ]);
+
+        SubCategory::create([
+            'category_id' => 4,
+            'title' => 'Шампуни',
+            'url' => 'shampuni',
+        ]);
+        SubCategory::create([
+            'category_id' => 4,
+            'title' => 'Маски',
+            'url' => 'maski',
+        ]);
+        SubCategory::create([
+            'category_id' => 4,
+            'title' => 'Спреи и масла',
+            'url' => 'sprei-i-masla',
+        ]);
+
+        SubCategory::create([
+            'category_id' => 5,
+            'title' => 'Мыло',
+            'url' => 'mylo',
+        ]);
+        SubCategory::create([
+            'category_id' => 5,
+            'title' => 'бельди',
+            'url' => 'beldi',
+        ]);
+
+        SubCategory::create([
+            'category_id' => 6,
+            'title' => 'Подарочные наборы',
+            'url' => 'podarochnye-nabory',
+        ]);
+
+        SubCategory::create([
+            'category_id' => 6,
+            'title' => 'Aкции',
+            'url' => 'aktsii',
+        ]);
     }
 }
