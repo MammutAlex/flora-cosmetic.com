18322c8e8f5a477b2c14000bfb880111931fcd08
Alex <alexk.mammut@gmail.com>
2017-12-07 11:54:26 +0200

    Correct import category photo

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -15,32 +15,32 @@ class CategoriesTableSeeder extends Seeder
         Category::query()->truncate();
         Category::create([
             'name' => 'Косметика для лица',
-            'photo' => '/images/categories/Категория_ косметика для лица2.png',
+            'photo' => 'images/categories/Категория_ косметика для лица2.png',
             'url' => 'kosmetika-dlya-litsa-i-oblasti-shei',
         ]);
         Category::create([
             'name' => 'Уход и очищение кожи',
-            'photo' => '/images/categories/Категория_уход_и_очищение3.png',
+            'photo' => 'images/categories/Категория_уход_и_очищение3.png',
             'url' => 'uhod-i-ochischenie-kozhi-litsa',
         ]);
         Category::create([
             'name' => 'Косметика для тела',
-            'photo' => '/images/categories/Категория_косметика_для_тела.png',
+            'photo' => 'images/categories/Категория_косметика_для_тела.png',
             'url' => 'kosmetika-dlya-tela',
         ]);
         Category::create([
             'name' => 'Косметика для волос',
-            'photo' => '/images/categories/Категория_для_волос.png',
+            'photo' => 'images/categories/Категория_для_волос.png',
             'url' => 'kosmetika-dlya-volos',
         ]);
         Category::create([
             'name' => 'Мыло и бельди',
-            'photo' => '/images/categories/Категория_домашнее_мыло.png',
+            'photo' => 'images/categories/Категория_домашнее_мыло.png',
             'url' => 'mylo-i-beldi',
         ]);
         Category::create([
             'name' => 'Подарочные наборы и акции',
-            'photo' => '/images/categories/Категория_Наборы_акция.png',
+            'photo' => 'images/categories/Категория_Наборы_акция.png',
             'url' => 'podarochnye-nabory-i-aktsii',
         ]);
     }
