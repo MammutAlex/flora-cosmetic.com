2e60200f290c4229c6f6d1893fffeb85818b6c6d
Alex <alexk.mammut@gmail.com>
2018-01-04 11:18:18 +0200

    Correct logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -71,7 +71,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="/"><img src="/images/logo2.png" style="height: 30px" alt=""></a>
+            <a class="navbar-brand" href="/"><img src="/images/logo3.png" style="height: 30px" alt=""></a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
